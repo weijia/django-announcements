4546b7e9d5d8ef571e1e93cae5d4f85274b7cb1c
Patrick Altman <paltman@gmail.com>
2011-11-29 16:37:55 -0600

    Add crud views with pluggable view mixin
    - idea for this is to quickly standup a staff
      admin for announcements and provide your own
      authentication rules via the mixin.

--- announcements/views.py
+++ announcements/views.py
@@ -1,8 +1,14 @@
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
 
+from announcements.forms import AnnouncementForm
+from announcements.mixins import ProtectedMixin
 from announcements.models import Announcement
+from announcements.signals import announcement_created, announcement_updated, announcement_deleted
 
 
 def dismiss(request, pk):
@@ -30,3 +36,66 @@ def detail(request, pk):
     return TemplateResponse(request, "announcements/detail.html", {
         "announcement": announcement
     })
+
+
+class CreateAnnouncementView(CreateView, ProtectedMixin):
+    
+    model = Announcement
+    form_class = AnnouncementForm
+    
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.creator = self.request.user
+        self.object.save()
+        
+        announcement_created.send(
+            sender=self.object,
+            announcement=self.object,
+            request=self.request
+        )
+        return super(CreateAnnouncementView, self).form_valid(form)
+    
+    def get_success_url(self):
+        return reverse("announcements_list")
+
+
+class UpdateAnnouncementView(UpdateView, ProtectedMixin):
+    
+    model = Announcement
+    form_class = AnnouncementForm
+    
+    def form_valid(self, form):
+        response = super(UpdateAnnouncementView, self).form_valid(form)
+        announcement_updated.send(
+            sender=self.object,
+            announcement=self.object,
+            request=self.request
+        )
+        return response
+    
+    def get_success_url(self):
+        return reverse("announcements_list")
+
+
+class DeleteAnnouncementView(DeleteView, ProtectedMixin):
+    
+    model = Announcement
+    
+    def form_valid(self, form):
+        response = super(DeleteAnnouncementView, self).form_valid(form)
+        announcement_deleted.send(
+            sender=self.object,
+            announcement=self.object,
+            request=self.request
+        )
+        return response
+    
+    def get_success_url(self):
+        return reverse("announcements_list")
+
+
+class AnnouncementListView(ListView, ProtectedMixin):
+    
+    model = Announcement
+    queryset = Announcement.objects.all().order_by("-creation_date")
+    paginate_by = 50
