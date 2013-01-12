b313d8f3688bb6bf25b4c9268bccea38aca48368
Patrick Altman <paltman@gmail.com>
2013-01-12 16:14:07 -0600

    Point to correct README in setup.py

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README"),
+    long_description=read("README.md"),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="MIT",
