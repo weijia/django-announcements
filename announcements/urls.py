ad7132982aa5a49900b1b0d4fc06505254b09fa9
Patrick Altman <paltman@gmail.com>
2013-01-11 21:34:05 -0600

    Rename dismiss url to be consistent

--- announcements/urls.py
+++ announcements/urls.py
@@ -6,9 +6,9 @@ from announcements.views import DeleteAnnouncementView, AnnouncementListView
 
 urlpatterns = patterns("",
     url(r"announcement/(?P<pk>\d+)/$", detail, name="announcements_detail"),
-    url(r"announcement/(?P<pk>\d+)/hide/$", dismiss, name="announcement_dismiss"),
     
     url(r"announcement/create/$", CreateAnnouncementView.as_view(), name="announcements_create"),
+    url(r"announcement/(?P<pk>\d+)/hide/$", dismiss, name="announcements_dismiss"),
     url(r"announcement/(?P<pk>\d+)/update/$", UpdateAnnouncementView.as_view(), name="announcements_update"),
     url(r"announcement/(?P<pk>\d+)/delete/$", DeleteAnnouncementView.as_view(), name="announcements_delete"),
     url(r"", AnnouncementListView.as_view(), name="announcements_list"),
